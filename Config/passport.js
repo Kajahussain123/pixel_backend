d40078291f69e6fe911d3329472f344fcf05357a
Kajahussain123 <kajahusain513@gmail.com>
2025-03-17 12:15:07 +0530

    updates

--- Config/passport.js
+++ Config/passport.js
@@ -9,7 +9,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://pixel-backend-83kw.onrender.com/api/user/auth/google/callback",
+        callbackURL: "https://plantmyad.com/api/user/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         console.log("Google Profile:", profile); // Check if Google returns data
